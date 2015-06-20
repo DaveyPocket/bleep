2102144461acb5a12650bfc14e013d3498f96f3b
Bradley Boccuzzi <bjboccuzzi@gmail.com>
2015-06-20 18:07:21 -0400

    Something more useful

--- bleep.go
+++ bleep.go
@@ -19,22 +19,36 @@ func main() {
 	
 	
 	termbox.Init()
+	defer termbox.Close()
 	termbox.SetInputMode(termbox.InputMouse)
-	var x, velx int
+	var x int
+	var velx float32
+	var x2, velx2 int
 	t.Start()
-
+	go ui()
 	for {
 		termbox.SetCell(x, 2, 'A', termbox.ColorDefault, termbox.ColorDefault)
+		termbox.SetCell(x2, 3, 'B', termbox.ColorDefault, termbox.ColorDefault)
+
 		termbox.Flush()
 		if x > 20 {
-			t.Buff.Data = beep(440, 4000)
-			velx = -1
-		}else if x < 3 {
-			t.Buff.Data = beep(880, 4000)
-			velx = 1
+			playBeep(&t.Buff, beep(110, 4000))
+			velx = -.5
+		}else if x < 10 {
+		playBeep(&t.Buff, beep(880, 4000))
+
+			velx = .5
+		}
+		if x2 > 20 {
+			playBeep(&t.Buff, beep(880, 4000))
+			velx2 = -1
+		}else if x2 < 10 {
+			playBeep(&t.Buff, beep(880, 4000))
+			velx2 = 1
 		}
 		x += velx
-		time.Sleep(100* time.Millisecond)
+		x2 += velx2
+		time.Sleep(50* time.Millisecond)
 		termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 	}
 
@@ -42,10 +56,32 @@ func main() {
 }
 
 
+func ui() {
+	for {
+	switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			if ev.Key == termbox.KeyEsc {
+				termbox.Close()
+			}
+	}
+	}
+}
+
+func playBeep(in *ringBuff, wave []int8){
+	tempIndex := in.Index
+	for i, _ := range wave{
+		in.Data[tempIndex] += wave[i]/4
+		tempIndex++
+		if tempIndex == in.Length{
+			tempIndex = 0
+		}
+	} 
+}
+
 func beep(freq, length int) (wave []int8) {
 	wave = make([]int8, length)
 	for n := 0; n < length; n++ {
-		wave[n] = int8(127*math.Sin(float64(freq*n)))
+		wave[n] = int8((1 - float64(n)/float64(length))*   127*math.Sin(float64(2*math.Pi)*float64(freq*n)/44100))
 	}
 	return wave
 }
